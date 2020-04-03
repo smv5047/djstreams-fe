eb708b877f053e8c93780f1ca0c8f62ce405daae
smv5047 <smv5047@gmail.com>
2020-04-03 13:34:22 -0400

    updated event card

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,12 +9,14 @@ export default function Card(props) {
         <h2>{props.event.event_name}</h2>
       </div>
       {/* <div>{props.event.artist}</div> */}
-      <div className="artists"></div>
+      <div className="artists">Artist: {props.event.description}</div>
       <div className="set-time">
-        <div>Start Time</div>
-        <div>End Time</div>
+        {/* <div>Start Time</div> */}
+        <div>{props.event.event_start}</div>
+        {/* <div>End Time</div> */}
+        <div>{props.event.event_end}</div>
       </div>
-      {/* <div>{props.event.description}</div> */}
+
       <div className="description">
         In this spirit, influential YouTube curation channel Proximity has
         teamed up with Los Angeles event company Brownies and Lemonade to
@@ -26,14 +28,14 @@ export default function Card(props) {
           <a href={props.event.link}>Link</a>
         </div>
         {/* <div>{props.event.headbops}</div> */}
-        <div>HEAD BOP IT</div>
+        {/* <div>HEAD BOP IT</div> */}
         {/* <div>{props.event.tracklist}</div> */}
-        <div>
+        {/* <div>
           <a href="https://www.1001tracklists.com/tracklist/238v18tt/sebastien-leger-beatport-live-reconnect-2020-03-28.html">
             Tracklist
           </a>
         </div>
-        <div>Set Reminder</div>
+        <div>Set Reminder</div> */}
       </div>
     </div>
   );
