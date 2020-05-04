0b6bd86bba97c47b3ee578c916ffda9de393ee46
smv5047 <smv5047@gmail.com>
2020-05-03 22:00:06 -0400

    removed servicework comment

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-//update serviceWorker to .register
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
