e341b796637e79cc779827c5005fc6491494c22b
smv5047 <smv5047@gmail.com>
2020-06-12 15:37:26 -0400

    update serviceworker comments

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
+//Update serviceworker
 ReactDOM.render(
   <Router>
     <App />
