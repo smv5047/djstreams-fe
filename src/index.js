ac61513aedbfe5a1f2912339978c202eb8d6748c
smv5047 <smv5047@gmail.com>
2020-06-22 21:46:26 -0400

    index updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
-//Serviceworker update
 ReactDOM.render(
   <Router>
     <App />
