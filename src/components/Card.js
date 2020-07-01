f35d9c6dafd7524b8b4546b965286f227271ceb8
smv5047 <smv5047@gmail.com>
2020-07-01 17:07:58 -0400

    dRYed up code

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import "./Card.scss";
 
-let startdate = "startdate";
-
 export default function Card(props) {
   let newStartDate = Date.parse(props.event.event_start);
   let formattedStartDate = new Date(newStartDate).toLocaleDateString();
