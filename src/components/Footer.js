ffef398f37c333429de3a6aeed8e372cb6f3bf12
smv5047 <smv5047@gmail.com>
2020-07-16 22:50:07 -0400

    comments updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-//removed link to avoid warning that would make the build fail
+
 import "./Footer.scss";
 
 function Footer() {
